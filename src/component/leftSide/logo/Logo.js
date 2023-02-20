a5008138e0ce2adc081ff19736f5a85c6fb931b8
Rasha Daghlass <rasha.daghlass955@gmail.com>
2023-02-20 13:33:26 +0200

    first commit

---
+++ src/component/leftSide/logo/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <>
+    <img src="./assets/logo.png" alt="userpilot logo " />
+    </>
+  )
+}
+
+export default Logo
---
+++ src/views/Homepage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Logo from '../component/leftSide/logo/Logo'
+
+const Homepage = () => {
+  return (
+    <Logo/>
+  )
+}
+
+export default Homepage
